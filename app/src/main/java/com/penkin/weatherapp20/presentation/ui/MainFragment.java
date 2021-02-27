f44215d0fde3475953269573a708a13345eafe55
Oleg <49286750+Daganep@users.noreply.github.com>
2021-02-27 17:28:33 +0300

    Orientation lock.

--- app/src/main/java/com/penkin/weatherapp20/presentation/ui/MainFragment.java
+++ app/src/main/java/com/penkin/weatherapp20/presentation/ui/MainFragment.java
@@ -1,10 +1,22 @@
 package com.penkin.weatherapp20.presentation.ui;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.penkin.weatherapp20.R;
 import com.penkin.weatherapp20.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
 
     private FragmentMainBinding mainBinding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
 }
