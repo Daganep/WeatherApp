f5136fe796e5a7be44ae28d0bca6876b33cf8d1e
Oleg <49286750+Daganep@users.noreply.github.com>
2021-03-01 15:13:49 +0300

    Main fragment moxy init.

--- app/src/main/java/com/penkin/weatherapp20/view/Main/MainFragment.java
+++ app/src/main/java/com/penkin/weatherapp20/view/Main/MainFragment.java
@@ -6,19 +6,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 
-import com.penkin.weatherapp20.R;
 import com.penkin.weatherapp20.databinding.FragmentMainBinding;
+import com.penkin.weatherapp20.model.entities.OpenWeatherResponse;
+import com.penkin.weatherapp20.presenter.MainPresenter;
 
 import moxy.MvpAppCompatFragment;
+import moxy.presenter.InjectPresenter;
 
 public class MainFragment extends MvpAppCompatFragment implements MainView {
 
+    @InjectPresenter
+    MainPresenter presenter;
     private FragmentMainBinding mainBinding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        mainBinding = FragmentMainBinding.inflate(inflater, container, false);
+        return mainBinding.getRoot();
     }
+
+    @Override
+    public void viewUpdate(OpenWeatherResponse response) {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mainBinding = null;
+    }
+
+
 }
--- app/src/main/java/com/penkin/weatherapp20/view/Main/MainView.java
+++ app/src/main/java/com/penkin/weatherapp20/view/Main/MainView.java
@@ -1,12 +1,13 @@
 package com.penkin.weatherapp20.view.Main;
 
-import java.util.List;
+import com.penkin.weatherapp20.model.entities.OpenWeatherResponse;
 
 import moxy.MvpView;
 import moxy.viewstate.strategy.AddToEndSingleStrategy;
 import moxy.viewstate.strategy.StateStrategyType;
 
 public interface MainView extends MvpView {
-    /*@StateStrategyType(value = AddToEndSingleStrategy.class)
-    void updateRecyclerView(List<Element> elements);*/
+
+    @StateStrategyType(value = AddToEndSingleStrategy.class)
+    void viewUpdate(OpenWeatherResponse response);
 }
