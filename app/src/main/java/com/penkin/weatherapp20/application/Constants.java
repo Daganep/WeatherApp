05ba3ed7fdefefbb5fff8d058dc87cfd01bf1cef
Oleg <49286750+Daganep@users.noreply.github.com>
2021-03-01 19:27:26 +0300

    MainFragment view change.

--- app/src/main/java/com/penkin/weatherapp20/application/Constants.java
+++ app/src/main/java/com/penkin/weatherapp20/application/Constants.java
@@ -2,4 +2,6 @@ package com.penkin.weatherapp20.application;
 
 public class Constants {
     public static final String APIKEY = "762ee61f52313fbd10a4eb54ae4d4de2";
+    public static final String IMAGE_PATH = "https://openweathermap.org/img/wn/";
+    public static final String IMAGE_FORMAT = "@2x.png";
 }
