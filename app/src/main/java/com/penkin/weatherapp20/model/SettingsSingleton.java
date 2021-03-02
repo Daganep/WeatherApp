86c6e305d0c4fab160d45638532b5eaf44c33c29
Oleg <49286750+Daganep@users.noreply.github.com>
2021-03-02 10:48:23 +0300

    SettingsSingleton add.

---
+++ app/src/main/java/com/penkin/weatherapp20/model/SettingsSingleton.java
@@ -0,0 +1,35 @@
+package com.penkin.weatherapp20.model;
+
+public final class SettingsSingleton {
+
+    private static SettingsSingleton settings;
+
+    private SettingsSingleton() {
+    }
+
+    public static SettingsSingleton getSettingsSingleton(){
+        if(settings == null){
+            settings = new SettingsSingleton();
+        }
+        return settings;
+    }
+
+    private static String currentCity = ""; //current city from choose
+    private static String locationCity = ""; //current location city
+
+    public static String getCurrentCity() {
+        return currentCity;
+    }
+
+    public static void setCurrentCity(String currentCity) {
+        SettingsSingleton.currentCity = currentCity;
+    }
+
+    public static String getLocationCity() {
+        return locationCity;
+    }
+
+    public static void setLocationCity(String locationCity) {
+        SettingsSingleton.locationCity = locationCity;
+    }
+}
