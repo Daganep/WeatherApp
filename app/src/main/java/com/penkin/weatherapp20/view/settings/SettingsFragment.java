bdedb0cb9e11f079c2d237177d12bd3a89f086ea
Oleg <49286750+Daganep@users.noreply.github.com>
2021-03-08 18:44:37 +0300

    Settings layout init.

--- app/src/main/java/com/penkin/weatherapp20/view/settings/SettingsFragment.java
+++ app/src/main/java/com/penkin/weatherapp20/view/settings/SettingsFragment.java
@@ -1,10 +1,41 @@
 package com.penkin.weatherapp20.view.settings;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.penkin.weatherapp20.databinding.FragmentSettingsBinding;
 
+import java.util.Objects;
+
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding settingsBinding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        settingsBinding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return settingsBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+    }
+
+    private void init(View view){
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(settingsBinding.settingsToolbar);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
 }
