1ebc2a8af1a87c0c804bbb615d3c6f4190ac7836
Oleg <49286750+Daganep@users.noreply.github.com>
2021-03-08 19:04:45 +0300

    Click listener in settings fragment add.

--- app/src/main/java/com/penkin/weatherapp20/view/settings/SettingsFragment.java
+++ app/src/main/java/com/penkin/weatherapp20/view/settings/SettingsFragment.java
@@ -1,20 +1,26 @@
 package com.penkin.weatherapp20.view.settings;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
+import com.penkin.weatherapp20.R;
+import com.penkin.weatherapp20.application.Constants;
 import com.penkin.weatherapp20.databinding.FragmentSettingsBinding;
 
 import java.util.Objects;
 
-public class SettingsFragment extends Fragment {
+import moxy.MvpAppCompatFragment;
+
+public class SettingsFragment extends MvpAppCompatFragment implements SettingsView, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     private FragmentSettingsBinding settingsBinding;
 
@@ -32,10 +38,52 @@ public class SettingsFragment extends Fragment {
 
     private void init(View view){
         initToolbar();
+        clickListenerInit();
     }
 
     private void initToolbar(){
         ((AppCompatActivity) requireActivity()).setSupportActionBar(settingsBinding.settingsToolbar);
         Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void clickListenerInit(){
+        settingsBinding.celButtonSetFrag.setOnClickListener(this);
+        settingsBinding.farButtonSetFrag.setOnClickListener(this);
+        settingsBinding.winterThemeSetFrag.setOnClickListener(this);
+        settingsBinding.springThemeSetFrag.setOnClickListener(this);
+        settingsBinding.acceptButtonSetFrag.setOnClickListener(this);
+    }
+
+    //Log.d will be deleted, there will be button action
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.celButtonSetFrag:{
+                Log.d(Constants.TAG, "Celsius button clicked!");
+                break;
+            }
+            case R.id.farButtonSetFrag:{
+                Log.d(Constants.TAG, "Fahrenheit button clicked!");
+                break;
+            }
+            case R.id.winterThemeSetFrag:{
+                Log.d(Constants.TAG, "Violet button clicked!");
+                break;
+            }
+            case R.id.springThemeSetFrag:{
+                Log.d(Constants.TAG, "Green button clicked!");
+                break;
+            }
+            case R.id.acceptButtonSetFrag:{
+                Log.d(Constants.TAG, "Accept button clicked!");
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        //WeatherSettings.setNotificationON(isChecked);
     }
 }
---
+++ app/src/main/java/com/penkin/weatherapp20/view/settings/SettingsView.java
@@ -0,0 +1,6 @@
+package com.penkin.weatherapp20.view.settings;
+
+import moxy.MvpView;
+
+public interface SettingsView extends MvpView {
+}
